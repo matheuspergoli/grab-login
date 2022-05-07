31191d8bdcc620d50be447e36a9d3aad6d291f34
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-05-07 04:05:04 -0300

    Construindo formulário

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,20 @@ function handleSubmit(event) {
   const senha = form.password
 
   if (!email.value) {
+    email.classList.add('invalido')
     email.nextElementSibling.innerText = 'Por favor, insira um email válido.'
     return
   } else {
+    email.classList.remove('invalido')
     email.nextElementSibling.innerText = ''
   }
 
   if (!senha.value) {
+    senha.classList.add('invalido')
     senha.nextElementSibling.innerText = 'Por favor, insira uma senha.'
     return
   } else {
+    senha.classList.remove('invalido')
     senha.nextElementSibling.innerText = ''
   }
 
